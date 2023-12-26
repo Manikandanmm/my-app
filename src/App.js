c8393ce902d54cd5b6517defa9a0d5fb8890b1e1
Manikandanmm <113236931+Manikandanmm@users.noreply.github.com>
2023-12-26 19:35:35 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import react from 'react';
 
 
 import Section from "./component/section";
@@ -7,7 +8,7 @@ function App() {
   return (
     <div className="App">
 
-
+        <h1>Hello world</h1>
         <Section/>
         
         
